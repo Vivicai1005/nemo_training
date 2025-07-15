d04ac52f3bd01934e9cd37da709e5a45d058afbd
Vivicai1005 <caiweiwei1005@gmail.com>
2025-07-15 14:44:05 +0800

    edit llama3 8b pretrain in test

--- test/pretrain_llama3_8b.py
+++ test/pretrain_llama3_8b.py
@@ -1,6 +1,13 @@
 import nemo_run as run
 
+from typing import List
 from nemo.collections import llm
+from nemo.collections.llm.recipes.llama3_8b import MegatronCommOverlapCallback
+from lightning.pytorch.callbacks.callback import Callback
+from nemo.collections.llm.recipes.precision.mixed_precision import bf16_with_fp8_mixed
+from nemo.lightning.pytorch.callbacks.flops_callback import FLOPsMeasurementCallback
+from nemo.utils import logging
+
 
 def local_executor_torchrun(nodes: int = 1, devices: int = 2) -> run.LocalExecutor:
     # Env vars for jobs are configured here
@@ -21,8 +28,27 @@ def run_pretraining():
         name="llama3_pretraining",
         num_nodes=1,
         num_gpus_per_node=8)
-    executor = local_executor_torchrun(nodes=recipe.trainer.num_nodes, devices=recipe.trainer.devices)
 
+    recipe.trainer.strategy.tensor_model_parallel_size = 1
+    recipe.trainer.strategy.pipeline_model_parallel_size = 1
+    recipe.trainer.strategy.context_parallel_size = 1
+    recipe.trainer.strategy.virtual_pipeline_model_parallel_size = None
+    recipe.data.global_batch_size = 128
+    recipe.data.micro_batch_size = 2
+    recipe.data.seq_length = 8192
+    recipe.trainer.plugins = bf16_with_fp8_mixed()
+    recipe.trainer.plugins.grad_reduce_in_fp32 = False
+
+    recipe.trainer.callbacks.append(
+        run.Config(
+            FLOPsMeasurementCallback,
+            model_config=recipe.model.config,
+            data_config=recipe.data,
+            model_name="llama3",
+        )
+    )
+
+    executor = local_executor_torchrun(nodes=recipe.trainer.num_nodes, devices=recipe.trainer.devices)
     run.run(recipe, executor=executor, name="llama3_8b_pretraining")
 
 # This condition is necessary for the script to be compatible with Python's multiprocessing module.
